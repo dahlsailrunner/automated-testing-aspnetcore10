62243c1d0652125257369d082ea7a30ed1e6a793
Erik Dahl <erik.dahl@realpage.com>
2026-07-27 05:16:30 -0500

    removed a redundant validation

--- CarvedRock.Domain/ProductValidator.cs
+++ CarvedRock.Domain/ProductValidator.cs
@@ -23,7 +23,6 @@ public class NewProductValidator : AbstractValidator<NewProductModel>
 
         RuleFor(p => p.Name)
             .NotEmpty().WithMessage("{PropertyName} is required.")
-            .NotNull().WithMessage("{PropertyName} is required.")
             .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
             //.MustAsync(NameIsUnique).WithMessage("A product with the same name already exists.");
 
