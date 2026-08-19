0140af7c1ff5f040be99b8014ee2f410ad79d9ee
Erik Dahl <erik.dahl@realpage.com>
2026-08-19 08:14:33 -0500

    added another playwright test

--- tests/CarvedRock.AppTests/Utils/CustomPageTest.cs
+++ tests/CarvedRock.AppTests/Utils/CustomPageTest.cs
@@ -1,3 +1,4 @@
+using TUnit.Core.Interfaces;
 using TUnit.Playwright;
 
 namespace CarvedRock.AppTests.Utils;
@@ -14,8 +15,43 @@ public class CustomPageTest : PageTest
         var options = base.ContextOptions(testContext);
         options.IgnoreHTTPSErrors = true;       
 
+        if (testContext.StateBag.ContainsKey(RecordVideoAttribute.StateBagKey))
+        {
+            options.RecordVideoDir = "playwright-artifacts/";
+
+            options.ViewportSize = new ViewportSize
+            { Width = 1280, Height = 1400 };
+        }
+
         return options;
     }
 
     public string WebAppUrl => Fixture.App.GetEndpoint("webapp").ToString();
+}
+
+public static class PageExtensions
+{
+    public static async Task Login(this IPage page, string username, string password)
+    {
+        await page.GetByRole(AriaRole.Textbox, new() { Name = "Username" })
+                                .FillAsync(username);
+        await page.GetByRole(AriaRole.Textbox, new() { Name = "Password" })
+                                .ClickAsync();
+        await page.GetByRole(AriaRole.Textbox, new() { Name = "Password" })
+                                .FillAsync(password);
+        await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+    }
+}
+
+// The StateBag is used to avoid reflection and use TUnit source generation approach
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class RecordVideoAttribute : Attribute, ITestDiscoveryEventReceiver
+{
+    internal const string StateBagKey = "CarvedRock.RecordVideo";
+
+    public ValueTask OnTestDiscovered(DiscoveredTestContext discoveredTestContext)
+    {
+        discoveredTestContext.TestContext.StateBag[StateBagKey] = true;
+        return default;
+    }
 }
--- tests/CarvedRock.AppTests/WebAppTests.cs
+++ tests/CarvedRock.AppTests/WebAppTests.cs
@@ -7,9 +7,61 @@ public partial class WebAppTests : CustomPageTest
     {
         await Page.GotoAsync(WebAppUrl);
 
+        await Page.ScreenshotAsync(new() { Path = "playwright-artifacts/screenshot.png" });
+
         await Expect(Page).ToHaveTitleAsync("Carved Rock Fitness");
 
         var bannerTextLocator = Page.GetByText("GET A GRIP");
         await Expect(bannerTextLocator).ToBeVisibleAsync();
+    }
+
+    [Test]
+    [RecordVideo]
+    public async Task CustomerCanPlaceOrderAndGetEmail()
+    {
+        await Page.GotoAsync(WebAppUrl);
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Footwear" }).ClickAsync();
+
+        // footwear link should redirect to login page
+        await Page.Login("alice", "alice");  // customer
+
+        await Page.GetByRole(AriaRole.Row, new() { Name = "Desert Walker" })
+                    .GetByRole(AriaRole.Button).ClickAsync();
+        await Page.GetByRole(AriaRole.Row, new() { Name = "River Guide" })
+                    .GetByRole(AriaRole.Button).ClickAsync();
+
+        // implicit assertion that the cart button shows 2 items in it
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Cart (2)" }).ClickAsync();
+
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Desert Walker");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("River Guide");
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Checkout" })
+                    .ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Submit Order" })
+                    .ClickAsync();
+
+        await Expect(Page.Locator("h1"))
+                .ToContainTextAsync("Thanks for your (fake) order!");
+
+        var emailUrl = Fixture.App.GetEndpoint("smtp", "http").ToString();
+
+        await Page.GotoAsync(emailUrl);
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "to: alicesmith@email.com" })
+                    .ClickAsync();
+
+        // playwright assertions against the email
+        await Expect(Page.Locator("#preview-html").ContentFrame
+                    .Locator("body")).ToContainTextAsync("Desert Walker");
+        await Expect(Page.Locator("#preview-html").ContentFrame
+                    .Locator("body")).ToContainTextAsync("River Guide");
+
+        await Expect(Page.Locator("#preview-html").ContentFrame
+                    .GetByRole(AriaRole.Heading))
+                    .ToContainTextAsync("Thank you for your order!");
+        await Expect(Page.Locator("#preview-html").ContentFrame.Locator("body"))
+                    .ToContainTextAsync("Enjoy your new gear!");
     }
 }
